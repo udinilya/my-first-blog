67af9875614d210005cfc9d825dbb9557e3596d6
ilya <udin76@bk.ru>
2022-01-25 13:18:45 +0300

    added security

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,5 +123,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGIN_REDIRECT_URL = '/'
